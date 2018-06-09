89bf0ac7e65a08c0f1c87a457302f3ffaa85b602
Ali1213 <yusili2000@163.com>
2018-06-09 13:31:20 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -60,10 +60,8 @@ export default {
     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
     }
-    console.log(111)
     axios.get('http://localhost:8000/getAll')
     .then((response) =>{
-      console.log(response)
       const data = response.data.data;
 
       this.tableData = data.map(item => {
