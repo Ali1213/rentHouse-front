5491d1ca6841eaa9b74fe687a5343042673da9e3
Ali1213 <yusili2000@163.com>
2018-06-09 13:33:13 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -36,49 +36,59 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   data() {
     return {
-      tableData: [
-      ]
+      tableData: []
     };
   },
-  mounted () {
-    Date.prototype.Format = function (fmt) {  
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
+  mounted() {
+    Date.prototype.Format = function(fmt) {
+      var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        S: this.getMilliseconds() //毫秒
+      };
+      if (/(y+)/.test(fmt))
+        fmt = fmt.replace(
+          RegExp.$1,
+          (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+        );
+      for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+          fmt = fmt.replace(
+            RegExp.$1,
+            RegExp.$1.length == 1
+              ? o[k]
+              : ("00" + o[k]).substr(("" + o[k]).length)
+          );
+      return fmt;
     };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-    }
-    axios.get('http://localhost:8000/getAll')
-    .then((response) =>{
-      const data = response.data.data;
+    axios
+      .get("http://localhost:8000/getAll")
+      .then(response => {
+        const data = response.data.data;
 
-      this.tableData = data.map(item => {
-
-        return {
-          title: item.title,
-          url: item.url,
-          content: item.content.trim().slice(0,50),
-          username: item.username,
-          create_time: new Date(item.create_time*1000).Format("yyyy-MM-dd hh:mm:ss"),
-        }
+        this.tableData = data.map(item => {
+          return {
+            title: item.title,
+            url: item.url,
+            content: item.content.trim().slice(0, 50),
+            username: item.username,
+            create_time: new Date(item.create_time * 1000).Format(
+              "yyyy-MM-dd hh:mm:ss"
+            )
+          };
+        });
       })
-    })
-    .catch(function(error) {
-      console.log(error)
-    });
-
+      .catch(function(error) {
+        console.log(error);
+      });
   },
   methods: {
     startHacking() {
